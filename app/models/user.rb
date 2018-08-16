885ca7a94cb752b2def2b305a3558dd4ae96625a
github username <github email address>
2018-08-16 18:09:41 -0500

    adding validations and omni auth gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :reviews
-  has_many :companies, :through=> :reviews 
+  has_many :companies, :through=> :reviews
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+
+  has_secure_password 
 end
