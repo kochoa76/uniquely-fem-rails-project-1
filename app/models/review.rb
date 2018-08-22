9a4002d975d83f72fb8b451e5c04d5ccb1acd248
github username <github email address>
2018-08-22 17:45:16 -0500

    adding to reviews form_with_co

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,12 +11,12 @@ class Review < ApplicationRecord
     self.company ? self.company.name : nil
   end
 
-  # def company_size = (size)
-  #   self.company = Company.find_or_create_by(size: size)
-  # end
-  #
-  # def company_size
-  #   self.company? self.company.size : nil
-  # end
+  def company_size=(size)
+    self.company = Company.create(size: size)
+  end
+
+  def company_size
+    self.company ? self.company.size : nil
+  end
 
 end
